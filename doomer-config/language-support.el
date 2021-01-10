a559ef05cda6c753611fc23e5f9d02bf44177847
Rephobia <26533256+Rephobia@users.noreply.github.com>
2021-01-10 21:22:17 +0300

    Remove lsp
    ccls includes subr-x package, it needs to avoid error:
    "Symbol's function definition is void: string-trim"

--- doomer-config/language-support.el
+++ doomer-config/language-support.el
@@ -7,66 +7,6 @@
   
   )
 
-
-(use-package lsp-mode
-  
-  ;; https://github.com/emacs-lsp/lsp-mode
-  
-  :ensure t
-  :delight
-  
-  :bind
-  (:map doomer/keymap
-	("M-f d" . xref-find-definitions-other-window)
-	)
-  
-  :hook
-  (c++-mode . lsp)
-  
-  :config
-  (setq lsp-enable-on-type-formatting nil
-	lsp-prefer-flymake nil)
-  
-  :commands
-  lsp
-  
-  )
-
-
-(use-package company-lsp
-  
-  ;; https://github.com/tigersoldier/company-lsp
-  
-  :ensure t
-  :commands company-lsp
-  
-  :config
-  (setq company-lsp-enable-snippet t
-	company-lsp-enable-recompletion t
-  	company-lsp-async t
-	company-lsp-cache-candidates nil)
-  (push 'company-lsp company-backends)
-  
-  )
-
-
-(use-package ccls
-
-  ;; https://github.com/MaskRay/ccls
-  ;; https://github.com/MaskRay/emacs-ccls
-
-  
-  :ensure t
-  :delight
-  :hook ((c-mode c++-mode objc-mode) .
-         (lambda () (require 'ccls) (lsp)))
-  
-  :config
-  (setq ccls-executable "~/.emacs.d/ccls/Release/ccls"
-	ccls-sem-highlight-method 'nil)
-  )
-
-
 (use-package js2-mode
   
   ;; https://github.com/mooz/js2-mode/
