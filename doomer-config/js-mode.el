f0a917ae9bce841af52736970a0102d2376f15ab
Rephobia <26533256+Rephobia@users.noreply.github.com>
2022-09-16 22:55:22 +0300

    Add js-doc package

--- doomer-config/js-mode.el
+++ doomer-config/js-mode.el
@@ -7,3 +7,11 @@
   (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
   
   )
+
+(use-package js-doc
+  :ensure t
+
+  :hook ((js2-mode . (lambda ()
+		       (define-key js2-mode-map (kbd "<C-tab>") 'js-doc-insert-function-doc)
+		       (define-key js2-mode-map "@" 'js-doc-insert-tag))))
+)
