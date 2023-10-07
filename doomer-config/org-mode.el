06b20b6797e4d837a445fd7a1e05d5a069c3f6ec
Rephobia <26533256+Rephobia@users.noreply.github.com>
2023-10-07 20:48:08 +0300

    Add org-todo-with-sort function

--- doomer-config/org-mode.el
+++ doomer-config/org-mode.el
@@ -1,4 +1,17 @@
 (use-package org-mode
   :ensure org-plus-contrib
   :mode (("\\.org$" . org-mode))
+
+  :bind
+  (:map doomer/keymap
+	("C-c C-t" . era/org-todo-with-sort)
+	)
+  :init
+
+  (defun era/org-todo-with-sort ()
+    (interactive)
+    (org-todo)
+    (mark-whole-buffer)
+    (org-sort-entries nil ?o nil nil nil)
+    )
   )
